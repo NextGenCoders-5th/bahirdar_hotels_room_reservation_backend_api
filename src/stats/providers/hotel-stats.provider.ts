3dd767a617c0300c27a4f9432c33bd6b929b8797
eziraa <ezratgab@gmail.com>
2025-07-19 14:02:11 +0300

    update statics logic

--- src/stats/providers/hotel-stats.provider.ts
+++ src/stats/providers/hotel-stats.provider.ts
@@ -2,17 +2,9 @@ import { Request, Response } from 'express';
 import { Types } from 'mongoose';
 import ReviewModel from '../../review/review.model';
 import RoomModel from '../../rooms/room.model';
-import UserModel from '../../users/users.model';
 import BookingModel from '../../bookings/bookings.model';
 
-// manager stats
-// cashier stats
-/* 
-    hotel related
-    number of all bookings, rooms, cashiers, reviews
 
-    // number of booking per day
-  */
 export async function hotelStatsProvider(req: Request, res: Response) {
   console.log('hotel-stats.provider...');
   try {
@@ -20,9 +12,7 @@ export async function hotelStatsProvider(req: Request, res: Response) {
     const allRooms = await RoomModel.countDocuments({
       hotel: new Types.ObjectId(id),
     });
-    const allUsers = await UserModel.countDocuments({
-      hotel: new Types.ObjectId(id),
-    });
+
     const allReviews = await ReviewModel.countDocuments({
       hotel: new Types.ObjectId(id),
     });
@@ -50,14 +40,30 @@ export async function hotelStatsProvider(req: Request, res: Response) {
       },
     ]);
 
+    const allBookedUsers = await BookingModel.aggregate([
+      {
+        $match: { hotel: new Types.ObjectId(id) },
+      },
+      {
+        $group: {
+          _id: '$user',
+          numOfBookings: { $sum: 1 },
+        },
+      }
+    ]);
+
+
+
+
+
     res.status(200).json({
       status: 'success',
       message: 'get all hotel stats',
       data: {
         allRooms,
-        allUsers,
+        allUsers: allBookedUsers.length,
         allReviews,
-        allBookings,
+        allBookings: allBookings.length,
       },
     });
   } catch (err) {
